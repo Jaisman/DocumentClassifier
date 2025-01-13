72ca490be9732a407491ae84bbe6ba427b26908e
Jaisman <jaismanjhinger@gmail.com>
2025-01-13 15:19:49 +0530

    Fixed port issue

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def classify():
     return jsonify({'prediction': prediction[0]}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
